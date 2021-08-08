49c59e53476daca1059d416a5b44622a80f4e321
OmaxrAli <omar.abdelmajeed17@gmail.com>
2021-08-09 00:40:35 +0300

    problem is solved

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 
 
 
-function object(location,min ,max ,AvgCookie, random ) {
+function object(location,min ,max ,AvgCookie ) {
 
     this.location=location;
     this.min= min;
     this.max = max;
     this.AvgCookie = AvgCookie;
-    this.random=random;
+
    
   }
-
-
-  
 
   function getRandom(min, max) {
         min = Math.ceil(1);
@@ -30,23 +27,29 @@ for (let index = 0; index < objectNum; index++) {
     let min= prompt("enter the min");
     let max= prompt("enter the max");
     let AvgCookie= prompt("enter the avg cookie sales");
-    let random=getRandom(1,21);
-    let me= new object(location, min, max, AvgCookie, random)
-    array.push(me)
-    let rand= AvgCookie*random;
-    totalSum.push(rand)
-
-}
-
-for (let index = 0; index < 12; index++) {
-    let rand= AvgCookie*random;
-    totalSum.push(rand)
     
+    let me= new object(location, min, max, AvgCookie)
+    array.push(me)
+
+    for (let index = 0; index < 12; index++) {
+     let rand= AvgCookie*getRandom(1,21);
+    totalSum.push(rand)
+        
+    }
+    
+
 }
 let sum =0;
-for (let i= 0; i< array.length;i++) {
+for ( j = 0; j < array.length; j++) {
+
     
+    console.log( array[j] );
+
+    for ( i= 0 ; i< totalSum.length ;i++) {
+    
+    console.log("Cookies per hour "+ totalSum[i]);
     sum = sum + totalSum[i];
-    console.log(array[i]);
+    }
+    console.log("total is "+sum);
 }
-console.log("total is "+sum);
+
